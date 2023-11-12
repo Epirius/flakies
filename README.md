a3cd2ad09ab1aa62e5445ba3c67eb5cd3bd63a58
Felix Kaasa <mrKaasa@protonmail.com>
2023-11-12 07:43:27 +0100

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+using this article to set up my config:
+https://cola-gang.industries/nixos-for-the-confused-part-i
+
+
+sudo nixos-rebuild switch --flake .#nixos
+home-manager switch --flake .#felix@nixos
---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+ description = "My Flakie"; #You can change this to whatever
+
+ inputs = {
+   # Nixpkgs
+   nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+   # Home manager
+   home-manager.url = "github:nix-community/home-manager";
+   home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+   # Hardware
+   hardware.url = "github:nixos/nixos-hardware";
+ };
+
+ outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+   # NixOS configuration entrypoint
+   # Available through 'nixos-rebuild --flake .#your-hostname'
+
+   nixosConfigurations = {
+     nixos = nixpkgs.lib.nixosSystem {
+       specialArgs = { inherit inputs; }; # Pass flake inputs to our config
+       # > Our main nixos configuration file <
+       modules = [ 
+         ./nixos/configuration.nix
+       ];
+     };
+   };
+
+   # home-manager configuration entrypoint
+   # Available through 'home-manager --flake .#your-username@your-hostname'
+   homeConfigurations = {
+     "felix@nixos" = home-manager.lib.homeManagerConfiguration {
+       pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+       extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+       # > Our main home-manager configuration file <
+       modules = [ 
+           ./home-manager/home.nix 
+           ];
+     };
+   };
+ };
+}
