75160ba7be0d8ee8743cef9a2a887b9b3fbd3bc3
Felix Kaasa <mrKaasa@protonmail.com>
2023-11-15 08:28:35 +0100

    updating hardware

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -6,13 +6,13 @@
 {
   imports = [ ];
 
-  boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules = [ "ohci_pci" "ehci_pci" "ahci" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/5849b165-00df-4af2-9763-0b698ce92e55";
+    { device = "/dev/disk/by-uuid/f222513b-ded1-49fa-b591-20ce86a2fe7f";
       fsType = "ext4";
     };
 
