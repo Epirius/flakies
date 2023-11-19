d4d245f720bd4e7c093b32b66dcabd19ede48fde
Felix Kaasa <mrKaasa@protonmail.com>
2023-11-19 19:22:51 +0100

    updating install script

--- full-install.sh
+++ full-install.sh
@@ -1,33 +1,37 @@
 #!/usr/bin/env nix-shell
+#!nix-shell -i bash -p git home-manager
+username=$(whoami)
 
 # NB: This script should be used on a clean install of nixos
 # where not even the git repo has been cloned yet.
 
 # Check if sudo
-if [ "$EUID" -ne 0 ]; then
-	echo "This script requires sudo privileges. Please run it with sudo."
+if [ "$EUID" -eq 0 ]; then
+	echo "Please do not run this script as root."
 	exit 1
 fi
 
-#!nix-shell -i bash -p git home-manager
 
 # Cloning the repo
-cd ~
+cd /home/$username
 git clone https://github.com/Epirius/flakies.git
 cd flakies
 
+
 # Updating hardware config
-rm ./nixos/hardware-configuration.nix
+sudo rm ./nixos/hardware-configuration.nix
 sudo cp /etc/nixos/hardware-configuration.nix ./nixos/
 
 # Bootstrapping system
 export NIX_CONFIG="experimental-features = nix-command flakes"
-rm /home/felix/.mozilla/firefox/profiles.ini
+rm /home/$username/.mozilla/firefox/profiles.ini
 sudo nixos-rebuild switch --flake .#nixos
-home-manager switch --flake .#felix@nixos
+home-manager switch --flake .#$username@nixos
+
 
 rustup default stable
 nix-collect-garbage -d
+
 exit 0
 
 
